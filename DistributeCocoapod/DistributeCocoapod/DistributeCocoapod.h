0972b20ec70ba7dc8800490aba86b083c05f95af
KHUONG_ACV <khuongld@atmarkcafe.org>
2018-03-29 09:31:23 +0700

    new code

---
+++ DistributeCocoapod/DistributeCocoapod/DistributeCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  DistributeCocoapod.h
+//  DistributeCocoapod
+//
+//  Created by KHUONG_ACV on 3/29/18.
+//  Copyright © 2018 KHUONG_ACV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DistributeCocoapod.
+FOUNDATION_EXPORT double DistributeCocoapodVersionNumber;
+
+//! Project version string for DistributeCocoapod.
+FOUNDATION_EXPORT const unsigned char DistributeCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DistributeCocoapod/PublicHeader.h>
+
+
---
+++ DistributeCocoapod/DistributeCocoapod/FRNetwork.swift
@@ -0,0 +1,19 @@
+//
+//  FRNetwork.swift
+//  DistributeCocoapod
+//
+//  Created by KHUONG_ACV on 3/29/18.
+//  Copyright © 2018 KHUONG_ACV. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+public class FRNetwork: NSObject {
+    
+    public func getData () {
+        print("Called")
+    }
+}
